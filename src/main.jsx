ac03288f690c12aa6d86d19b8fd208f5d7624bf3
Pavan Kumar Kola <150538694+Pawankumar-01@users.noreply.github.com>
2025-12-18 17:52:18 +0530

    Update ReactDOM to use createRoot method

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import '@livekit/components-styles';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
